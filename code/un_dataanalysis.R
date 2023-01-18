d8d4b16dfe8fc5de1a2b604b52074f325594322e
yeshapatel-cornell <ysp6@cornell.edu>
2023-01-18 12:09:15 -0500

    learned how to join tables

--- code/un_dataanalysis.R
+++ code/un_dataanalysis.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(readr)
 gapminder_data <- read_csv("data/gapminder_data.csv")
 view(gapminder_data)
 summarize(gapminder_data, averageLifeExp=mean(lifeExp), medianLifeExp=median(lifeExp))
@@ -45,3 +46,35 @@ gapminder_data%>%
 gapminder_data%>%
   select (country, continent, year, lifeExp)%>%
   pivot_wider(names_from = year, values_from = lifeExp)
+
+# working with messy data
+
+co2_emmission_dirty <- read_csv("data/co2-un-data.csv", skip = 2,
+         col_names = c("region","country","year","series","value","footnotes","source"))
+
+co2_emissions <- co2_emmission_dirty %>%
+  select(country, year, series, value) %>%
+mutate (series = recode(series, "Emissions (thousand metric tons of carbon dioxide)" = "total_emissions",
+                        "Emissions per capita (metric tons of carbon dioxide)" = "per_capita_emissions")) %>%
+  pivot_wider(names_from=series, values_from=value) %>%
+  filter (year == 2005) %>%
+  select (-year)
+ 
+#Bringing in 2007 population data
+
+gapminder_data_2007 <- read_csv("data/gapminder_data.csv") %>%
+  filter (year == 2007) %>%
+  select (country, pop, lifeExp, gdpPercap)
+
+# joining two dataframes together
+
+# innerjoin- discards countries not common between the tables
+
+inner_join(co2_emissions, gapminder_data_2007, by = "country")
+
+anti_join(co2_emissions, gapminder_data_2007, by = "country")
+
+anti_join(gapminder_data_2007, co2_emissions, by ="country")  
+
+full_join(co2_emissions, gapminder_data_2007)%>%
+  view
